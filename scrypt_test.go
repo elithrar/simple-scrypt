8542d1224d8a0c3bd2f6a308511f126153bb3d25
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-10-21 21:38:22 +0200

    Travis needs even more error margin for timings
    [tests] Reverted tests back to the same package.

--- scrypt_test.go
+++ scrypt_test.go
@@ -1,12 +1,10 @@
-package scrypt_test
+package scrypt
 
 import (
 	"fmt"
 	"reflect"
 	"testing"
 	"time"
-
-	"github.com/elithrar/simple-scrypt"
 )
 
 // Test cases
@@ -17,21 +15,21 @@ var (
 
 var testParams = []struct {
 	pass   bool
-	params scrypt.Params
+	params Params
 }{
-	{true, scrypt.Params{16384, 8, 1, 32, 64}},
-	{true, scrypt.Params{16384, 8, 1, 16, 32}},
-	{true, scrypt.Params{65536, 8, 1, 16, 64}},
-	{true, scrypt.Params{1048576, 8, 2, 64, 128}},
-	{false, scrypt.Params{-1, 8, 1, 16, 32}},          // invalid N
-	{false, scrypt.Params{0, 8, 1, 16, 32}},           // invalid N
-	{false, scrypt.Params{1 << 31, 8, 1, 16, 32}},     // invalid N
-	{false, scrypt.Params{16384, 0, 12, 16, 32}},      // invalid R
-	{false, scrypt.Params{16384, 8, 0, 16, 32}},       // invalid R > maxInt/128/P
-	{false, scrypt.Params{16384, 1 << 24, 1, 16, 32}}, // invalid R > maxInt/256
-	{false, scrypt.Params{1 << 31, 8, 0, 16, 32}},     // invalid p < 0
-	{false, scrypt.Params{4096, 8, 1, 5, 32}},         // invalid SaltLen
-	{false, scrypt.Params{4096, 8, 1, 16, 2}},         // invalid DKLen
+	{true, Params{16384, 8, 1, 32, 64}},
+	{true, Params{16384, 8, 1, 16, 32}},
+	{true, Params{65536, 8, 1, 16, 64}},
+	{true, Params{1048576, 8, 2, 64, 128}},
+	{false, Params{-1, 8, 1, 16, 32}},          // invalid N
+	{false, Params{0, 8, 1, 16, 32}},           // invalid N
+	{false, Params{1 << 31, 8, 1, 16, 32}},     // invalid N
+	{false, Params{16384, 0, 12, 16, 32}},      // invalid R
+	{false, Params{16384, 8, 0, 16, 32}},       // invalid R > maxInt/128/P
+	{false, Params{16384, 1 << 24, 1, 16, 32}}, // invalid R > maxInt/256
+	{false, Params{1 << 31, 8, 0, 16, 32}},     // invalid p < 0
+	{false, Params{4096, 8, 1, 5, 32}},         // invalid SaltLen
+	{false, Params{4096, 8, 1, 16, 2}},         // invalid DKLen
 }
 
 var testHashes = []struct {
@@ -49,7 +47,7 @@ var testHashes = []struct {
 
 func TestGenerateRandomBytes(t *testing.T) {
 	for _, v := range testLengths {
-		_, err := scrypt.GenerateRandomBytes(v)
+		_, err := GenerateRandomBytes(v)
 		if err != nil {
 			t.Fatalf("failed to generate random bytes")
 		}
@@ -58,7 +56,7 @@ func TestGenerateRandomBytes(t *testing.T) {
 
 func TestGenerateFromPassword(t *testing.T) {
 	for _, v := range testParams {
-		_, err := scrypt.GenerateFromPassword([]byte(password), v.params)
+		_, err := GenerateFromPassword([]byte(password), v.params)
 		if err != nil && v.pass == true {
 			t.Fatalf("no error was returned when expected for params: %+v", v.params)
 		}
@@ -66,45 +64,45 @@ func TestGenerateFromPassword(t *testing.T) {
 }
 
 func TestCompareHashAndPassword(t *testing.T) {
-	hash, err := scrypt.GenerateFromPassword([]byte(password), scrypt.DefaultParams)
+	hash, err := GenerateFromPassword([]byte(password), DefaultParams)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := scrypt.CompareHashAndPassword(hash, []byte(password)); err != nil {
+	if err := CompareHashAndPassword(hash, []byte(password)); err != nil {
 		t.Fatal(err)
 	}
 
-	if err := scrypt.CompareHashAndPassword(hash, []byte("invalid-password")); err == nil {
+	if err := CompareHashAndPassword(hash, []byte("invalid-password")); err == nil {
 		t.Fatalf("mismatched passwords did not produce an error")
 	}
 
 	invalidHash := []byte("$166$$11$a2ad56a415af5")
-	if err := scrypt.CompareHashAndPassword(invalidHash, []byte(password)); err == nil {
+	if err := CompareHashAndPassword(invalidHash, []byte(password)); err == nil {
 		t.Fatalf("did not identify an invalid hash")
 	}
 
 }
 
 func TestCost(t *testing.T) {
-	hash, err := scrypt.GenerateFromPassword([]byte(password), scrypt.DefaultParams)
+	hash, err := GenerateFromPassword([]byte(password), DefaultParams)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	params, err := scrypt.Cost(hash)
+	params, err := Cost(hash)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !reflect.DeepEqual(params, scrypt.DefaultParams) {
+	if !reflect.DeepEqual(params, DefaultParams) {
 		t.Fatal("cost mismatch: parameters used did not match those retrieved")
 	}
 }
 
 func TestDecodeHash(t *testing.T) {
 	for _, v := range testHashes {
-		_, err := scrypt.Cost([]byte(v.hash))
+		_, err := Cost([]byte(v.hash))
 		if err == nil && v.pass == false {
 			t.Fatal("invalid hash: did not correctly detect invalid password hash")
 		}
@@ -123,10 +121,10 @@ func TestCalibrate(t *testing.T) {
 		{1},
 	} {
 		var (
-			p   scrypt.Params
+			p   Params
 			err error
 		)
-		p, err = scrypt.Calibrate(timeout, tc.MemMiB, p)
+		p, err = Calibrate(timeout, tc.MemMiB, p)
 		if err != nil {
 			t.Fatalf("%d. %#v: %v", testNum, p, err)
 		}
@@ -134,7 +132,7 @@ func TestCalibrate(t *testing.T) {
 			t.Errorf("%d. wanted memory limit %d, got %d.", testNum, tc.MemMiB, (128*p.R*p.N)>>20)
 		}
 		start := time.Now()
-		_, err = scrypt.GenerateFromPassword([]byte(password), p)
+		_, err = GenerateFromPassword([]byte(password), p)
 		dur := time.Since(start)
 		t.Logf("GenerateFromPassword with %#v took %s (%v)", p, dur, err)
 		if err != nil {
@@ -142,17 +140,17 @@ func TestCalibrate(t *testing.T) {
 		}
 		if dur < timeout/2 {
 			t.Errorf("%d. GenerateFromPassword was too fast (wanted around %s, got %s) with %#v.", testNum, timeout, dur, p)
-		} else if timeout+timeout/10 < dur {
+		} else if timeout*2 < dur {
 			t.Errorf("%d. GenerateFromPassword took too long (wanted around %s, got %s) with %#v.", testNum, timeout, dur, p)
 		}
 	}
 }
 
 func ExampleCalibrate() {
-	p, err := scrypt.Calibrate(1*time.Second, 128, scrypt.Params{})
+	p, err := Calibrate(1*time.Second, 128, Params{})
 	if err != nil {
 		panic(err)
 	}
-	dk, err := scrypt.GenerateFromPassword([]byte("super-secret-password"), p)
+	dk, err := GenerateFromPassword([]byte("super-secret-password"), p)
 	fmt.Printf("generated password is %q (%v)", dk, err)
 }
