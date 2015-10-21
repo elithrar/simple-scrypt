5836e2684324fa54c37d14d83ef93baaf68513c4
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-10-21 21:38:22 +0200

    Travis needs even more error margin for timings

--- scrypt_test.go
+++ scrypt_test.go
@@ -142,7 +142,7 @@ func TestCalibrate(t *testing.T) {
 		}
 		if dur < timeout/2 {
 			t.Errorf("%d. GenerateFromPassword was too fast (wanted around %s, got %s) with %#v.", testNum, timeout, dur, p)
-		} else if timeout+timeout/10 < dur {
+		} else if timeout*2 < dur {
 			t.Errorf("%d. GenerateFromPassword took too long (wanted around %s, got %s) with %#v.", testNum, timeout, dur, p)
 		}
 	}
